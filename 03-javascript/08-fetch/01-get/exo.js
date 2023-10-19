34f6d2ad87772feac8a1be62843c80127069bc7c
Timothée Cresson <timotheecresson@hotmail.fr>
2023-10-19 16:01:31 +0200

    fetch

--- 03-javascript/08-fetch/01-get/exo.js
+++ 03-javascript/08-fetch/01-get/exo.js
@@ -1,5 +1,5 @@
 "use strict"
-
+const header = document.querySelector("header")
 const url = "./hero.json";
 const heroes = document.querySelector("#heroes")
 /*
@@ -17,15 +17,29 @@ function members(response) {
     if (response.ok) {
         response.json()
             .then(data=>{
-                data.members.forEach(members => {
+                data.members.forEach(heroesName => {
                     const option = document.createElement("option");
-                    option.value = members.name;
-                    option.textContent = members.name 
-                    heroes.appendChild(option);
+                    option.value = heroesName.name;
+                    option.textContent = heroesName.name 
+                    heroes.append(option);
                     heroes.removeAttribute("disabled");
                     console.log(option);
+
+                    
+
+
+                    option.addEventListener('click', optionMembers)
+                    function optionMembers() {
+                        const card = document.createElement("div");
+                        header.append(card);
+                        const inCard = document.createElement("ul")
+                        card.append(inCard);
+                        const liCard = document.createElement("li")
+                        inCard.append(liCard);
+                        liCard.innerHTML +=`${data.members}`
+                        
+                    }
                 });
-                
             })
             .catch(error=>console.error(error))
         }
@@ -34,3 +48,51 @@ function members(response) {
         }
 }
 
+heroes.addEventListener("change", () => {
+                const selectedHeroes = Array.from(heroes.selectedOptions, option => option.value);
+                displayHeroCards(selectedHeroes, data.members);})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*heroesSelect.addEventListener("change", () => {
+                const selectedHeroes = Array.from(heroesSelect.selectedOptions, option => option.value);
+                displayHeroCards(selectedHeroes, data.members);
+            });
+        })
+        .catch(error => console.error("Une erreur s'est produite lors de la requête fetch : " + error));
+
+    // Fonction pour afficher les cartes des héros sélectionnés sous forme de liste
+    function displayHeroCards(selectedHeroes, allHeroes) {
+        heroInfoList.innerHTML = "";
+
+        selectedHeroes.forEach(heroName => {
+            const selectedHero = allHeroes.find(hero => hero.name === heroName);
+
+            if (selectedHero) {
+                const heroItem = document.createElement("li");
+                heroItem.innerHTML = `
+                    <h2>${selectedHero.name}</h2>
+                    <p>Âge : ${selectedHero.age}</p>
+                    <p>Identité secrète : ${selectedHero.secretIdentity}</p>
+                    <p>Pouvoirs : ${selectedHero.powers.join(", ")}</p>
+                `;
+                heroInfoList.appendChild(heroItem);
+            }
+        });
+        */
