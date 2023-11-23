6f72bc8e05019e842f85ee1c525200ba3e90e635
Timothée Cresson <timotheecresson@hotmail.fr>
2023-11-23 15:54:01 +0100

    docker

---
+++ 05-docker/04-compose/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Découverte de docker Compose</title>
+</head>
+<body>
+    <h1>Bravo, votre docker compose fonctionne bien</h1>
+    <h2>Bienvenue sur <?php echo $_ENV["SUPER_ENVIRONNEMENT_VARIABLE"]??"" ?></h2>
+</body>
+</html>
+
+<!-- Pour démarrer on écrit dans navigation http://localhost:8080/ -->
