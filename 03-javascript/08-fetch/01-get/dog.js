85ddecc5a0582c986433e090b0d136dc2eefbe2c
Timothée Cresson <timotheecresson@hotmail.fr>
2023-12-08 16:16:06 +0100

    tim 2e projet

--- 03-javascript/08-fetch/01-get/dog.js
+++ 03-javascript/08-fetch/01-get/dog.js
@@ -6,6 +6,9 @@ fetch( "https://api.thedogapi.com/v1/images/search").then
             const img = document.createElement('img');
             img.src= data[0].url;
             document.body.appendChild(img)
+            const texte = document.createElement ("p")
+            texte.textContent = data[0].id;
+            document.body.append(texte)
         })
     }
 })
